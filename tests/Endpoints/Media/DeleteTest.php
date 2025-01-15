2e53eeeccf832aa4a95a07d658b00ac8dc36b2b3
EdgarsN <neimanis.edgars@gmail.com>
2025-01-15 10:55:27 +0200

    change withData to withQuery on media delete (#10)
    * change withData to withQuery on media delete
    
    * fix Media/Delete endpoint tests

--- tests/Endpoints/Media/DeleteTest.php
+++ tests/Endpoints/Media/DeleteTest.php
@@ -11,11 +11,11 @@ class DeleteTest extends TestCase
 {
     public function test_with_single_id(): void
     {
-        $this->makeBasicAuthEndpointTest(new Delete(123), [], ['id' => [123]]);
+        $this->makeBasicAuthEndpointTest(new Delete(123), ['id' => [123]]);
     }
 
     public function test_it_accepts_multiple_ids(): void
     {
-        $this->makeBasicAuthEndpointTest(new Delete([123, 456]), [], ['id' => [123, 456]]);
+        $this->makeBasicAuthEndpointTest(new Delete([123, 456]), ['id' => [123, 456]]);
     }
 }
