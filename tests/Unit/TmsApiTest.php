3c7164581cecc69c88e5ab1e864e45e4ff2ab169
edgars.neimanis <neimanis.edgars@gmail.com>
2025-03-03 15:40:08 +0200

    update timeout and middleware definition to allow per request usage

--- tests/Unit/TmsApiTest.php
+++ tests/Unit/TmsApiTest.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Newman\LaravelTmsApiClient\Tests\Unit;
 
 use Illuminate\Contracts\Config\Repository as ConfigRepository;
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 use Newman\LaravelTmsApiClient\Auth\ApiKeyAuthMethod;
 use Newman\LaravelTmsApiClient\Client;
+use Newman\LaravelTmsApiClient\Contracts\ClientContract;
 use Newman\LaravelTmsApiClient\Exceptions\InvalidTmsApiClientException;
 use Newman\LaravelTmsApiClient\Support\Facades\TmsApi;
+use Newman\LaravelTmsApiClient\Tests\Support\GetTestEndpoint;
 use Newman\LaravelTmsApiClient\Tests\TestCase;
 
 class TmsApiTest extends TestCase
@@ -110,5 +114,56 @@ class TmsApiTest extends TestCase
         $this->expectExceptionMessage('TMS Api client auth credentials are not provided.');
 
         TmsApi::client('ipsum');
+    }
+
+    public function test_it_respects_individual_timeouts_on_client(): void
+    {
+        TmsApi::fake();
+
+        /** @var ConfigRepository $config */
+        $config = $this->app->make(ConfigRepository::class);
+
+        $config->set('tms-api.clients', [
+            'ipsum' => [
+                'auth' => [
+                    'username' => 'lorem',
+                    'password' => 'ipsum',
+                ],
+
+                'http' => [
+                    'timeout' => 1,
+                    'connect_timeout' => 1,
+                ],
+            ],
+        ]);
+
+        $runClientTest = function (ClientContract $client, int $timeout, int $connection_timeout) {
+            $actualClientTimeout = null;
+            $actualClientConnectionTimeout = null;
+
+            $clientHttpFactory = $client->buildHttpFactory();
+            $clientHttpFactory->preventStrayRequests();
+            $clientHttpFactory->fake(function (Request $request, array $options) use (&$actualClientTimeout, &$actualClientConnectionTimeout) {
+                $actualClientTimeout = $options['timeout'];
+                $actualClientConnectionTimeout = $options['connect_timeout'];
+
+                if ($request->url() == 'https://api.cloudycdn.services/api/v5/Test?status=ingested') {
+                    return Http::response(['msg' => '', 'code' => 0]);
+                }
+
+                return Http::response('----', 404);
+            });
+
+            $client
+                ->timeout($timeout)
+                ->connectTimeout($connection_timeout)
+                ->run(new GetTestEndpoint);
+
+            $this->assertEquals($timeout, $actualClientTimeout);
+            $this->assertEquals($connection_timeout, $actualClientConnectionTimeout);
+        };
+
+        $runClientTest(TmsApi::client('ipsum'), 15, 15);
+        $runClientTest(TmsApi::client('ipsum'), 20, 20);
     }
 }
