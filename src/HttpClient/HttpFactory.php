00659136dd81d2d92d9e33ec6afc31871afa5150
Edgars Neimanis <neimanis.edgars@gmail.com>
2025-03-19 15:13:04 +0200

    update httpfactory fake to clear stubCallbacks

--- src/HttpClient/HttpFactory.php
+++ src/HttpClient/HttpFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Newman\LaravelBackscreenApiClient\HttpClient;
 
 use Illuminate\Http\Client\Factory;
+use Illuminate\Support\Collection;
 
 class HttpFactory extends Factory
 {
@@ -15,6 +16,19 @@ class HttpFactory extends Factory
      */
     protected function newPendingRequest()
     {
-        return new PendingRequest($this);
+        return (new PendingRequest($this))->preventStrayRequests(false);
+    }
+
+    /**
+     * Register a stub callable that will intercept requests and be able to return stub responses.
+     *
+     * @param  callable(\Illuminate\Http\Client\Request $request, array<string, mixed> $options): (\Closure|\GuzzleHttp\Promise\PromiseInterface|\Illuminate\Http\Client\Response|null)|array<string, int|string|\Closure|\Illuminate\Http\Client\Response|\Illuminate\Http\Client\ResponseSequence|\GuzzleHttp\Promise\PromiseInterface>|null  $callback
+     * @return $this
+     */
+    public function fake($callback = null)
+    {
+        $this->stubCallbacks = new Collection;
+
+        return parent::fake($callback);
     }
 }
